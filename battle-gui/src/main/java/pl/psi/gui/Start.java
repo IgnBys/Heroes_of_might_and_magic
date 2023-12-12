df6e427b60bf4049acfcb31ee6993c7c8d57875b
IgnBys <ignatbyshnev@gmail.com>
2023-12-13 00:10:21 +0100

    Added css file and spells on the right position in the spell book

--- battle-gui/src/main/java/pl/psi/gui/Start.java
+++ battle-gui/src/main/java/pl/psi/gui/Start.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import pl.psi.Hero;
@@ -59,24 +60,23 @@ public class Start extends Application
                 .getResource( "fxml/SpellWindow.fxml" ) );
 
         try {
-//            Stage primaryStage = new Stage();
-//            Scene scene1 = null;
-//            Label label = new Label("Welcome to scene1");
-//            Button button1 = new Button("Back to scene");
-//            button1.setOnAction(e -> primaryStage.close());
-//            VBox layout = new VBox(20);
-//            layout.getChildren().addAll(label, button1);
-//            AnchorPane anchorPane = loader.load();
-//            anchorPane.setStyle("-fx-background-image: url('https://upload.wikimedia.org/wikibooks/ru/thumb/7/73/Heroes-of-Might-Magic-III-spell-book.jpg/300px-Heroes-of-Might-Magic-III-spell-book.jpg');");
-//            scene1 = new Scene(layout, 500, 500);
-//            primaryStage.setScene(scene1);
-//            primaryStage.show();
-            AnchorPane anchorPane = loader.load();
-            Scene scene1 = new Scene(anchorPane, 500, 500);
+            GridPane gridPane = loader.load();
             Stage primaryStage = new Stage();
             primaryStage.initModality(Modality.APPLICATION_MODAL);
-            Button button1 = (Button) anchorPane.lookup("#button1");
+            Button button1 = (Button) gridPane.lookup("#button1");
+            Button button2 = (Button) gridPane.lookup("#button2");
+            Button button3 = (Button) gridPane.lookup("#button3");
+            Button button4 = (Button) gridPane.lookup("#button4");
+//            GridPane.setRowIndex(button1, 0);
+//            GridPane.setColumnIndex(button1, 0);
+//            GridPane.setRowIndex(button2, 0);
+//            GridPane.setColumnIndex(button2, 1);
             button1.setOnAction(e ->primaryStage.close());
+            button2.setOnAction(e ->primaryStage.close());
+            button3.setOnAction(e ->primaryStage.close());
+            button4.setOnAction(e ->primaryStage.close());
+            Scene scene1 = new Scene(gridPane, 500, 500);
+
             primaryStage.setScene(scene1);
             primaryStage.showAndWait();
         }   catch (IOException e) {
